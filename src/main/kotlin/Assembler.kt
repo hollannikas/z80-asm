2972808184cfa2e8c5bb418d7ab4697374c2e51e
Rudolf Poels <rudolf.poels@futurice.com>
2020-03-14 18:29:38 +0200

    add simple operand parsing

--- src/main/kotlin/Assembler.kt
+++ src/main/kotlin/Assembler.kt
@@ -1,16 +1,17 @@
-
 data class SourceLine(
     val label: String,
     val mnemonic: String,
-    val operands: String,
-    val comment: String) {
+    val operands: List<String>,
+    val comment: String
+) {
     companion object {
         fun toSourceLine(line: String): SourceLine {
             val label = getLabel(line)
             val containsLabel = label.isNotEmpty()
-            val mnemonic =  getMnemonic(line, containsLabel)
-            
-            val operands = ""
+            val mnemonic = getMnemonic(line, containsLabel)
+
+            val operands = getOperands(line, containsLabel)
+
             val comment = ""
             return SourceLine(label, mnemonic, operands, comment)
         }
@@ -18,11 +19,20 @@ data class SourceLine(
         private fun getLabel(line: String) =
             line.substringBefore(':').let { candidate ->
                 if (candidate.isEmpty()) throw IllegalArgumentException()
-                if (candidate.length == line.length) "" else candidate 
+                if (candidate.length == line.length) "" else candidate
             }
-        
+
         private fun getMnemonic(line: String, containsLabel: Boolean) =
             if (containsLabel) line.split(" ")[1]
             else line.split(" ").first()
+
+        private fun getOperands(line: String, containsLabel: Boolean): List<String> {
+            val splitLine = line.split(" ")
+            return if (splitLine.size == 1
+                || (containsLabel && splitLine.size == 2)) emptyList()
+            else splitLine[if (containsLabel) 2 else 1]
+                .split(",")
+        }
+
     }
 }
--- src/test/kotlin/SourceLineTest.kt
+++ src/test/kotlin/SourceLineTest.kt
@@ -71,3 +71,54 @@ internal class SourceLineMnemonicTest {
         }
     }
 }
+
+internal class SourceLineOperandsTest {
+
+    @Test
+    fun `when one operand is given, it is parsed`() {
+        val line = "PUSH DE"
+        val sourceLine = SourceLine.toSourceLine(line)
+
+        assertEquals(listOf("DE"), sourceLine.operands)
+    }
+
+    @Test
+    fun `when two operands are given, they are parsed`() {
+        val line = "ADD HL,SP"
+        val sourceLine = SourceLine.toSourceLine(line)
+
+        assertEquals(listOf("HL","SP"), sourceLine.operands)
+    }
+
+    @Test
+    fun `when three operands are given, they are parsed`() {
+        val line = "LD A,(IX+01)"
+        val sourceLine = SourceLine.toSourceLine(line)
+
+        assertEquals(listOf("A","(IX+01)"), sourceLine.operands)
+    }
+ 
+    @Test
+    fun `when an operand is given after a label, it is parsed`() {
+        val line = "label: CALL C,8000"
+        val sourceLine = SourceLine.toSourceLine(line)
+
+        assertEquals(listOf("C","8000"), sourceLine.operands)
+    }
+
+    @Test
+    fun `when no operands are given, the list of operands is empty`() {
+        val line = "SCF"
+        val sourceLine = SourceLine.toSourceLine(line)
+
+        assertEquals(emptyList<String>(), sourceLine.operands)
+    }
+
+    @Test
+    fun `when no operands are given after a label, the list of operands is empty`() {
+        val line = "label: RRCA"
+        val sourceLine = SourceLine.toSourceLine(line)
+
+        assertEquals(emptyList<String>(), sourceLine.operands)
+    }
+}
